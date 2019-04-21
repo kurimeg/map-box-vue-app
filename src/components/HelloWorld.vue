c4e5eefd98aec5e521a8a1b3701f0f370f97a3a5
Megumi Kurihara <pomme.c5.musique@gmail.com>
2019-04-21 22:24:08 +0900

    東京駅中心地図表示

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <MglMap :accessToken="accessToken" :mapStyle="mapStyle" :center="center" :zoom="zoom" />
+</template>
+
+<script>
+import Mapbox from "mapbox-gl";
+import { MglMap } from "vue-mapbox";
+
+export default {
+  components: {
+    MglMap
+  },
+  data() {
+    return {
+      accessToken: '',
+      mapStyle: 'mapbox://styles/mapbox/streets-v11',
+      center: [139.767125, 35.681236],
+      zoom: 9 
+    };
+  },
+
+  created() {
+    // We need to set mapbox-gl library here in order to use it in template
+    this.mapbox = Mapbox;
+  }
+};
+</script>
